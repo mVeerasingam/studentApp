c25861ee9ee64727b6d564027c617df8d0c156f6
G00403931 <G00403931@atu.ie>
2022-10-18 13:45:26 +0100

    change branch studentName to studentDetails

--- src/main/java/ie/atu/week5Lab/Student.java
+++ src/main/java/ie/atu/week5Lab/Student.java
@@ -5,8 +5,10 @@ public class Student {
     public static void main(String[] args) {
         //Create first instance of student name
         System.out.println("Please enter Student Name");
-        Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        Scanner input = new Scanner(System.in);         //create scanner
+        String name = input.nextLine();                 //read user input
+
+        System.out.println("You entered : " + name);
     }
 
 }
--- src/main/java/ie/atu/week5Lab/studentClass.java
+++ src/main/java/ie/atu/week5Lab/studentClass.java
@@ -2,5 +2,6 @@ package ie.atu.week5Lab;
 
 
 public class studentClass {
+    private String name;
 
 }
