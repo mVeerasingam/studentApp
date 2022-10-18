ed2c1b7d8a303d3c82fc76176608d0d8b19fa10f
G00403931 <G00403931@atu.ie>
2022-10-18 13:34:33 +0100

    Added new branch studentName

--- src/main/java/ie/atu/week5Lab/Student.java
+++ src/main/java/ie/atu/week5Lab/Student.java
@@ -1,4 +1,12 @@
 package ie.atu.week5Lab;
+import  java.util.Scanner;
 
 public class Student {
+    public static void main(String[] args) {
+        //Create first instance of student name
+        System.out.println("Please enter Student Name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+    }
+
 }
