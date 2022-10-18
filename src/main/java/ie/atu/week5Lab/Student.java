8a4dbb71413d72bce6031b7707906991e80dddaa
G00403931 <G00403931@atu.ie>
2022-10-18 14:26:59 +0100

    added 2nd and 3rd instance of student email, name and course

--- src/main/java/ie/atu/week5Lab/Student.java
+++ src/main/java/ie/atu/week5Lab/Student.java
@@ -3,9 +3,7 @@ import  java.util.Scanner;
 
 public class Student {
     public static void main(String[] args) {
-        //Create first instance of student email
-
-        //Create first instance of student name
+        //Created first instance of student name
         System.out.println("Please enter Student Name: ");
         Scanner input = new Scanner(System.in);         //create scanner
         String name = input.nextLine();                 //read user input
@@ -13,17 +11,55 @@ public class Student {
         studentDetails.setName(name);
         System.out.println("Name entered : " + studentDetails.getName());
 
-        //Create first instance of student email
+        //Created first instance of student email
         System.out.println("Please enter Student Email: ");
         String email = input.nextLine();
         studentDetails.setEmail(email);
         System.out.println("Email entered : " + studentDetails.getEmail());
 
-        //Create first instance of student course
+        //Created first instance of student course
         System.out.println("Please enter Student Course: ");
         String course = input.nextLine();
         studentDetails.setCourse(course);
         System.out.println("Course entered : " + studentDetails.getCourse());
+
+            //Created second instance of student name
+            StudentClass studentDetails2 = new StudentClass();
+            System.out.println("Please enter 2nd Student Name: ");
+            String name2 = input.nextLine();
+            studentDetails2.setName(name2);
+            System.out.println("Name Entered :" + studentDetails2.getName());
+
+            //Created second instance of student email
+            System.out.println("Please enter 2nd Student Email: ");
+            String email2 = input.nextLine();
+            studentDetails2.setEmail(email2);
+            System.out.println("Email entered :" + studentDetails2.getEmail());
+
+            //Created second instance of student course
+            System.out.println("Please enter 2nd Student Course: ");
+            String course2 = input.nextLine();
+            studentDetails2.setCourse(course2);
+            System.out.println("Course entered :" + studentDetails2.getCourse());
+
+                //Created third instance of student name
+                StudentClass studentDetails3 = new StudentClass();
+                System.out.println("Please enter 3rd Student Name: ");
+                String name3 = input.nextLine();
+                studentDetails3.setName(name3);
+                System.out.println("Name Entered :" + studentDetails3.getName());
+
+                //Created third instance of student email
+                System.out.println("Please enter 3rd Student Email: ");
+                String email3 = input.nextLine();
+                studentDetails3.setEmail(email3);
+                System.out.println("Email entered :" + studentDetails3.getEmail());
+
+                //Created third instance of student course
+                System.out.println("Please enter 3rd Student Course: ");
+                String course3 = input.nextLine();
+                studentDetails3.setCourse(course3);
+                System.out.println("Course entered :" + studentDetails3.getCourse());
     }
 
 }
