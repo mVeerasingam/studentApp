9b8af7d573a84e2527f2710adcf79415c58d1766
G00403931 <G00403931@atu.ie>
2022-10-18 14:14:46 +0100

    added first instance of student email and course

--- src/main/java/ie/atu/week5Lab/Student.java
+++ src/main/java/ie/atu/week5Lab/Student.java
@@ -3,13 +3,27 @@ import  java.util.Scanner;
 
 public class Student {
     public static void main(String[] args) {
+        //Create first instance of student email
+
         //Create first instance of student name
-        System.out.println("Please enter Student Name");
+        System.out.println("Please enter Student Name: ");
         Scanner input = new Scanner(System.in);         //create scanner
         String name = input.nextLine();                 //read user input
         StudentClass studentDetails = new StudentClass();
         studentDetails.setName(name);
         System.out.println("Name entered : " + studentDetails.getName());
+
+        //Create first instance of student email
+        System.out.println("Please enter Student Email: ");
+        String email = input.nextLine();
+        studentDetails.setEmail(email);
+        System.out.println("Email entered : " + studentDetails.getEmail());
+
+        //Create first instance of student course
+        System.out.println("Please enter Student Course: ");
+        String course = input.nextLine();
+        studentDetails.setCourse(course);
+        System.out.println("Course entered : " + studentDetails.getCourse());
     }
 
 }
--- src/main/java/ie/atu/week5Lab/StudentClass.java
+++ src/main/java/ie/atu/week5Lab/StudentClass.java
@@ -2,19 +2,22 @@ package ie.atu.week5Lab;
 
 
 public class StudentClass {
-    //private String email;
+    private String email;
     private String name;
-    //private String course;
-
+    private String course;
 
 
     public StudentClass()
     {
+        email="";
         name="";
+        course="";
     }
 
-    public StudentClass(String name) {
+    public StudentClass(String email, String name, String course) {
+        this.email = email;
         this.name = name;
+        this.course = course;
     }
 
     public String getName() {
@@ -23,5 +26,21 @@ public class StudentClass {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
     }
 }
